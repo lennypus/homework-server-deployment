1968989f1ad7878541fac1249009a46d83f91b53
lennypus <lenny.cipondoh@gmail.com>
2021-10-01 21:02:27 +0700

    change wording jumlah

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 		USERNAME := c.Param("USERNAME")
 		c.JSON(200, gin.H{
 			"status":  "success",
-			"message": "jumlah follower USERNAME = " + USERNAME,
+			"message": "total follower USERNAME = " + USERNAME,
 		})
 	})
 	router.GET("/:USERID", func(c *gin.Context) {
